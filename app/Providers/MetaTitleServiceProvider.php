402c0375d7cd0f94988e987d03677dd2b05bccbe
trr-code <154942624+trr-code@users.noreply.github.com>
2024-12-16 11:16:32 -0600

    Update MetaTitleServiceProvider.php

--- app/Providers/MetaTitleServiceProvider.php
+++ app/Providers/MetaTitleServiceProvider.php
@@ -1,1 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
+class MetaTitleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Pass the $metaTitle variable to all views
+        View::share('metaTitle', 'Blade Test');
+    }
+}
 
